5d6aa07111806ad9d477c8d5bb83faa85d2321a2
omarbanjo <omarbanjo@yahoo.com>
2019-10-18 09:21:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+ import { Cart, Med,Oma,Omm,Nam,Ommm,Namm,Ommmm } from './card';
+
+function App() {
+  return (
+    <div className="App">
+      <Cart/>
+      <Med/>
+       <Oma/> 
+      <Omm/>
+      <Nam/>
+      <Ommm/>
+      <Namm/>
+      <Ommmm/>
+    </div>
+  );
+}
+ export default App;
